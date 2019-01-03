d12122043f761de2779130be58af729333a9500f
jhab82 <bertrand.jan@gmail.com>
2019-01-03 15:58:41 +0100

    added restricted mode

--- mam_resume_publish_restricted.js
+++ mam_resume_publish_restricted.js
@@ -3,6 +3,9 @@ const IOTA = require('iota.lib.js');
 const iota = new IOTA({ provider: 'https://nodes.devnet.iota.org:443' });
 var fs = require('fs');
 
+//Choosing the sidekey for restricted communication
+const key = iota.utils.toTrytes("tmmiot-iota-sideky");
+var newChannelStarted = false;
 
 var readline = require('readline');
 var rl = readline.createInterface( {
@@ -13,15 +16,11 @@ var rl = readline.createInterface( {
 rl.question("Do you like to start a new channel (y/n)=", function(answer)  {
 	if (answer === "y") {
 		rl.close();
+            newChannelStarted = true;
 	        var mamState = Mam.init(iota, undefined, 1);
-        	mamState = Mam.changeMode(mamState, "public");
-		message = Mam.create(mamState, trytes);
-		mamState = message.state;
-		console.log("Ok - start over again");
-		//save the root of the channel
-		fs.writeFile("mamChannelRoot.json", JSON.stringify(mamState), function(err, data) {
-			if (err) { console.log("error: "+ err)}
-		});
+        	mamState = Mam.changeMode(mamState, "restricted", key);
+		    console.log("Ok - start over again");
+		    //save the root of the channel
 		executeDataPublishing(mamState)
 	    	setInterval(function() { executeDataPublishing(mamState); } , 15*1000);
 	} else if (answer ==="n") {
@@ -29,7 +28,7 @@ rl.question("Do you like to start a new channel (y/n)=", function(answer)  {
 		console.log("Ok - read the latest mamState.json");
 	        var mamState = Mam.init(iota, undefined, 1);
 		mamState = JSON.parse(fs.readFileSync("mamState.json"));
-		mamState = Mam.changeMode(mamState, "public");
+		mamState = Mam.changeMode(mamState, "restricted", key);
 		executeDataPublishing(mamState)
 	    setInterval(function() { executeDataPublishing(mamState); } , 15*1000);
 
@@ -46,12 +45,15 @@ async function publish(mamState, packet) {
     // Create MAM Payload
     const trytes = iota.utils.toTrytes(JSON.stringify(packet));
     const message = Mam.create(mamState, trytes);
-
     // Save new mamState
     mamState = message.state;
+    if (newChannelStarted == true) {
+        fs.writeFile("mamChannelRoot.json", JSON.stringify(mamState), function(err, data) {
+			if (err) { console.log("error: "+ err)}
+		});
+        newChannelStarted = false;
+    }
     console.log('Root: ', message.root);
-    console.log('Address: ', message.address);
-
     // Attach the payload.
     await Mam.attach(message.payload, message.address);
 
@@ -63,13 +65,10 @@ async function executeDataPublishing(mamState) {
     const json = {"id": "123", "time": "13.05.218", "pm2.5": "10", "pm10": "20"};
     const root = await publish(mamState, json);
 
-    console.log(JSON.stringify(mamState));
+    console.log("mamState: "+ JSON.stringify(mamState));
 
 	fs.writeFile("mamState.json", JSON.stringify(mamState), function(err, data) {
-		if (err) {
-                        console.log("err: " + err);
-                }
+		if (err) { console.log("err: " + err); }
         });
-
 }
 
