3b29d90fef6c5a05a8bf2ad290dc2c8fa78c7b39
jhab82 <bertrand.jan@gmail.com>
2019-01-03 17:58:36 +0100

    added sensor packages

--- tmmiot_mam_publish.js
+++ tmmiot_mam_publish.js
@@ -1,105 +1,89 @@
-/*
-Author: Robert Lie (mobilefish.com)
-
-The mam_publish.js file publishes random generated numbers on the tangle using MAM.
-This file will work on a computer or Raspberry Pi.
-The published data can be viewed using the mam_receive.js file or
-https://www.mobilefish.com/services/cryptocurrency/mam.html (Select option: Data receiver)
-
-Usage:
-1)  You can change the default settings: MODE, SIDEKEY, SECURITYLEVEL or TIMEINTERVAL
-    If you do, make the same changes in mam_receive.js file.
-2)  Start the app: node mam_publish.js
-
-More information:
-https://www.mobilefish.com/developer/iota/iota_quickguide_raspi_mam.html
-*/
-
 const Mam = require('./lib/mam.client.js');
 const IOTA = require('iota.lib.js');
-const moment = require('moment');
 const iota = new IOTA({ provider: 'https://nodes.devnet.iota.org:443' });
-
-//PM sensor package and init
-const Sensor = require('sds011-client');
-const sensor = new Sensor("/dev/ttyUSB0");
-
-//DHT sensor package and init
-
 var fs = require('fs');
 
-const MODE = 'restricted'; // public, private or restricted
-const SIDEKEY = 'bertrand.jan@gmail.com'; // Enter only ASCII characters. Used only in restricted mode
-const SECURITYLEVEL = 1; // 1, 2 or 3
-const TIMEINTERVAL  = 180; // seconds
+//Sensor support
+const rpiDhtSensor = require('rpi-dht-sensor'); //DHT sensor com package
+var dht = new rpiDhtSensor.DHT22(4); //on GPIO 4
 
-//const seed = "AECAMKYRRN9RKQWUQAPQEYODRVNNZFLRESMBIFZUZ9FEGASVDHVFBGTQAYMP9CZZDDVIUCNR9RSXEJWLU";
-//const seed = undefined;
+const Sensor = require('sds011-client');//PM sensor com package 
+const sensor = new Sensor("/dev/ttyUSB0"); // Use your system path of SDS011 sensor.
+
+sensor.setReportingMode('query'); //set the PM sensor to query mode
+sensor.setWorkingPeriod(3); //set the interval to 3min.
 
 
+//Choosing the sidekey for restricted communication
+const key = iota.utils.toTrytes("tmmiot-iota-sideky");
+var newChannelStarted = false;
 
+var readline = require('readline');
+var rl = readline.createInterface( {
+	input: process.stdin, 
+	output: process.stdout
+});
 
-// Initialise MAM State
-var mamState = Mam.init(iota, undefined,SECURITYLEVEL);
+rl.question("Do you like to start a new channel (y/n)=", function(answer)  {
+	if (answer === "y") {
+		rl.close();
+            newChannelStarted = true;
+	        var mamState = Mam.init(iota, undefined, 1);
+        	mamState = Mam.changeMode(mamState, "restricted", key);
+		console.log("Ok - start over again");
+		executeDataPublishing(mamState)
+	    	setInterval(function() { executeDataPublishing(mamState); } , 180*1000);
+	} else if (answer ==="n") {
+		rl.close();
+		console.log("Ok - read the latest mamState.json");
+	        var mamState = Mam.init(iota, undefined, 1);
+		mamState = JSON.parse(fs.readFileSync("mamState.json"));
+		mamState = Mam.changeMode(mamState, "restricted", key);
+		executeDataPublishing(mamState)
+	    setInterval(function() { executeDataPublishing(mamState); } , 180*1000);
 
-//console.log("first publish on new channel");
-//Read the channel information
-var mamState = JSON.parse(fs.readFileSync("mamState.json"));
+	} else {
+		console.log("Exit - please answer with y or n");
+		rl.close();
+		process.exit(1);
+	}
+});
 
-// Set channel mode
-
-if (MODE == 'restricted') {
-    const key = iota.utils.toTrytes(SIDEKEY);
-    mamState = Mam.changeMode(mamState, MODE, key);
-} else {
-    mamState = Mam.changeMode(mamState, MODE);
-}
 
 // Publish data to the tangle
-const publish = async function(packet) {
+async function publish(mamState, packet) {
     // Create MAM Payload
     const trytes = iota.utils.toTrytes(JSON.stringify(packet));
     const message = Mam.create(mamState, trytes);
-
     // Save new mamState
     mamState = message.state;
+    if (newChannelStarted == true) {
+        fs.writeFile("mamChannelRoot.json", JSON.stringify(mamState), function(err, data) {
+			if (err) { console.log("error: "+ err)}
+		});
+        newChannelStarted = false;
+    }
     console.log('Root: ', message.root);
-    console.log('Address: ', message.address);
-    //console.log('message: ', JSON.stringify(message))
-
-
     // Attach the payload.
     await Mam.attach(message.payload, message.address);
-    
-    // Write the the last state from this channel to the file
-	fs.writeFile("mamState.json", JSON.stringify(mamState), function(err, data) {
-    		if (err) {
-    			console.log("err: " + err);
-		} else {
-			console.log("data:" + JSON.stringify(data));
-		}
-	});
 
     return message.root;
 }
 
-const generateJSON = function() {
-    // Generate some random numbers simulating sensor data
-    const data = Math.floor((Math.random()*89)+10);
-    const dateTime = moment().utc().format('DD/MM/YYYY hh:mm:ss');
-    const json = {"data": data, "dateTime": dateTime, "owner": "jan"};
-    return json;
+
+async function executeDataPublishing(mamState) {
+
+	var readout = dht.read();
+
+	sensor.query().then( async function(data) {
+	    const json = {"id": "1", "time": Date.now(), "pm2.5": data.pm2p5, "pm10": data.pm10, "H": parseInt(readout.humidity), "T": parseInt(readout.temperature)};
+	   console.log("payload :" + JSON.stringify(json));
+	const root = await publish(mamState, json);
+
+	console.log("mamState: "+ JSON.stringify(mamState));
+	fs.writeFile("mamState.json", JSON.stringify(mamState), function(err, data) {
+		if (err) { console.log("err: " + err); }
+        });
+	});
 }
 
-const executeDataPublishing = async function() {
-    const json = generateJSON();
-    console.log("json=",json);
-
-    const root = await publish(json);
-    console.log(`dateTime: ${json.dateTime}, data: ${json.data}, root: ${root}`);
-}
-
-// Start it immediately
-//executeDataPublishing();
-
-setInterval(executeDataPublishing, TIMEINTERVAL*1000);
